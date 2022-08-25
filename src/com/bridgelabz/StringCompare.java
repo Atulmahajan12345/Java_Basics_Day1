0bce0bbde432513014f16fd02eb4fc29a32d4b1e
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-08-25 10:25:57 +0530

    Added:check two strings are equal or not

---
+++ src/com/bridgelabz/StringCompare.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class StringCompare {
+	public static void main(String[] args) {
+
+		String style1 = "umesh";
+		String style2 = "atul";
+
+		if (style1 == style2)
+			System.out.println("Equal");
+		else
+			System.out.println("Not Equal");
+	}
+
+}
